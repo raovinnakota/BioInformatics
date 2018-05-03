098281785246253241e7cbd9e059211e6ad6241c
raovinnakota <eswarv@sbcglobal.net>
2018-05-03 11:00:08 -0400

    Amino acid translated

--- Lab_Reports/Lab07/rao_vinnakota_lab07.py
+++ Lab_Reports/Lab07/rao_vinnakota_lab07.py
@@ -7,9 +7,10 @@ import random
 from SequenceVariation import SequenceVariation, random_sequence
 from protein_translate import list_convert
 
-seq1, seq2, seq3 = random_sequence(10), random_sequence(10), random_sequence(10)
+seq1, seq2, seq3 = random_sequence(20), random_sequence(20), random_sequence(20)
 Seq1, Seq2, Seq3 = SequenceVariation(seq1), SequenceVariation(seq2), SequenceVariation(seq3)
 aa1, aa2, aa3 = list_convert(Seq1.seqs), list_convert(Seq2.seqs), list_convert(Seq3.seqs)
+
 
 print(aa1)
 print(aa2)
